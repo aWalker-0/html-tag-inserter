440b4bfa58a48e1f6f0bf20dee731fa585b1ed77
aWalker-0 <awalkerpa@gmail.com>
2022-11-02 18:00:29 -0500

    Allows blank selection now; moves cursor back to end of selected text

--- out/tagInsert.js
+++ out/tagInsert.js
@@ -7,22 +7,19 @@ exports.getInsertText = exports.insertTag = void 0;
 const vscode = require("vscode");
 async function insertTag() {
     // Get input from user
-    const tag = await vscode.window.showInputBox();
+    const tag = await vscode.window.showInputBox({ prompt: "Enter in HTML tag" });
     // If tag is null or is empty, throw error
     if (tag === null || tag === "") {
         error("No empty tags permitted!");
+        return;
     }
     // Grab what the user has selected 
     let selection = vscode.window.activeTextEditor.selection;
-    // If the selection is not empty, replace selection with generated HTML tag and text that was inside
-    if (!selection.isEmpty) {
-        vscode.window.activeTextEditor.edit(builder => {
-            builder.replace(selection, getInsertText(vscode.window.activeTextEditor.document.getText(selection), tag));
-        });
-    }
-    else {
-        error("Your selection is empty!");
-    }
+    vscode.window.activeTextEditor.edit(builder => {
+        builder.replace(selection, getInsertText(vscode.window.activeTextEditor.document.getText(selection), tag));
+    });
+    // Move cursor to the end of the selected text
+    moveCursor(tag);
 }
 exports.insertTag = insertTag;
 // Generate and return tag with selected text inside
@@ -39,6 +36,22 @@ function getInsertText(selection, tag) {
     }
 }
 exports.getInsertText = getInsertText;
+function moveCursor(tag) {
+    // Define variable to store length of tag
+    let tagLength = tag?.length ?? 0;
+    // Unselect all pasted text
+    vscode.commands.executeCommand("cursorMove", {
+        to: "right",
+        by: "wrappedLine",
+        value: 0
+    });
+    // Move the cursor back to selection end
+    vscode.commands.executeCommand("cursorMove", {
+        to: "left",
+        by: "wrappedLine",
+        value: tagLength + 3
+    });
+}
 // Error function
 function error(text) {
     vscode.window.showErrorMessage(text);
--- src/tagInsert.ts
+++ src/tagInsert.ts
@@ -7,22 +7,23 @@ import * as vscode from 'vscode';
 export async function insertTag() {
 
     // Get input from user
-    const tag = await vscode.window.showInputBox();
+    const tag = await vscode.window.showInputBox({prompt: "Enter in HTML tag"});
     // If tag is null or is empty, throw error
     if(tag === null || tag! === "") {
         error("No empty tags permitted!");
+        return;
     }
 
     // Grab what the user has selected 
     let selection = vscode.window.activeTextEditor!.selection;
-    // If the selection is not empty, replace selection with generated HTML tag and text that was inside
-    if (!selection.isEmpty) {
-        vscode.window.activeTextEditor!.edit(builder => {
-            builder.replace(selection, getInsertText(vscode.window.activeTextEditor!.document.getText(selection), tag!));
-        });
-    } else {
-        error("Your selection is empty!");
-    }
+    vscode.window.activeTextEditor!.edit(builder => {
+        builder.replace(
+            selection, getInsertText(vscode.window.activeTextEditor!.document.getText(selection), tag!)
+        );
+    });
+    
+    // Move cursor to the end of the selected text
+    moveCursor(tag!);
 }
 
 // Generate and return tag with selected text inside
@@ -36,6 +37,25 @@ export function getInsertText(selection: string, tag: string) {
     } else {
         return "<" + tag +">" + selection + "</" + tag + ">";
     }
+}
+
+function moveCursor(tag: string) {
+    // Define variable to store length of tag
+    let tagLength = tag?.length ?? 0;
+
+    // Unselect all pasted text
+    vscode.commands.executeCommand("cursorMove", {
+        to: "right",
+        by: "wrappedLine",
+        value: 0
+    });
+
+    // Move the cursor back to selection end
+    vscode.commands.executeCommand("cursorMove", {
+        to: "left", 
+        by: "wrappedLine",
+        value: tagLength + 3
+    });
 }
 
 // Error function
